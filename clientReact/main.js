d3e3a16b38eea2d1b08ee69f5e1fe5d26beb3853
“JASON” <jrcorey84@gmail.com>
2017-06-14 13:19:44 -0500

    ES6 React

--- clientReact/main.js
+++ clientReact/main.js
@@ -89,72 +89,211 @@ var destination = document.querySelector('#container')
 
 // Lifecycle of Components
 
-var LifeComponent = React.createClass({
-	getDefaultProps: function(){
-		console.log('getting default props')
-	},
+// var LifeComponent = React.createClass({
+// 	getDefaultProps: function(){
+// 		console.log('getting default props')
+// 	},
 	
-	getInitialState: function(){
-		return{
-			count: 0,
-		}
-	},
+// 	getInitialState: function(){
+// 		return{
+// 			count: 0,
+// 		}
+// 	},
 	
-	componentWillMount: function(){
-		console.log(this.state)
-		console.log(this.props)
-		console.log('component is mounting')
-	},
+// 	componentWillMount: function(){
+// 		console.log(this.state)
+// 		console.log(this.props)
+// 		console.log('component is mounting')
+// 	},
 		
-	increment: function(){
+// 	increment: function(){
+// 		this.setState({
+// 			count: this.state.count + 1
+// 		})
+// 	},
+	
+// 	render: function(){
+// 		return(
+// 			<button onClick={this.increment}>{this.state.count}</button>
+// 		)
+// 	},
+
+// 	componentDidMount: function(){
+// 		console.log('component has rendered')
+// 		this.interval = setInterval(this.increment, 1000)
+// 	},
+
+// 	componentWillUnmount: function(){
+// 		clearInterval(this.interval)
+// 		console.log('component unmounted')
+// 	}
+// })
+
+// var LifecycleContainer = React.createClass({
+// 	mount: function(){
+// 		ReactDOM.render(
+// 			<LifeComponent />,
+// 			document.getElementById('renderHere')
+
+// 		)
+// 	},
+// 	unmount: function(){
+// 		ReactDOM.unmountComponentAtNode(
+// 			document.getElementById('renderHere')
+// 		)
+// 	},
+	
+// 	render: function(){
+// 		console.log('Lifecycle container rendered')
+// 		return(
+// 		<div>
+// 			<button onClick={this.mount}>Mount</button>
+// 			<button onClick={this.unmount}>Unmount</button>
+// 			<div id="renderHere"></div>
+// 		</div>
+		
+// 		)
+// 	}
+// })
+
+// Lifecycle Updates
+
+// var LikesComponent = React.createClass({
+// 	updateLikes: function(){
+// 		ReactDOM.render(
+// 			<LikesComponent likes={this.props.likes +1}/>,
+// 			destination
+// 		)
+// 	},
+	
+// 	getDefaultProps: function(){
+// 		return({
+// 			likes: 0,
+// 		})
+// 	},
+	
+// 	getInitialState: function(){
+// 		return({
+// 			popular: false,
+// 		})
+// 	},
+	
+// 	componentWillReceiveProps: function(nextProps){
+// 		console.log('nex props')
+// 		this.setState({
+// 			popular: nextProps.likes >= 10
+// 		})
+// 	},
+	
+// 	shouldComponentUpdate: function(nextProps,nextState){
+// 		return(
+// 			nextProps.likes % 2 === 0
+// 		)
+// 	},
+	
+// 	render: function(){
+// 		return(
+// 			<div className="container">
+// 				<h1>{this.state.popular ? "I'm popular" : "not popular"}</h1>
+// 				<button onClick={this.updateLikes}>Likes: {this.props.likes}</button>
+// 			</div>
+// 		)
+// 	},
+
+// 	componentDidUpdate: function(prevProps,prevState){
+// 		console.log('previous props', prevProps)
+// 		console.log('previous states', prevState)
+// 	}
+
+// })
+
+// ES6 Classes
+
+// class NameOfComponent extends React.Component {
+// 	constructor(props){
+// 		super(props);
+// 		this.state = {count: 0}
+// 		this.click = this.click.bind(this)
+// 	}
+// 	click(){
+// 		console.log('clicked')
+// 	}
+// 	render(){
+// 		return(
+			// <div onClick={ () => this.click(10) }>{this.props.text}</div>
+// 			<div className="container">
+// 				<Button clickHandler={this.click} text="Hello"></Button>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// NameOfComponent.defaultProps = {text: 'Bitch'}
+// NameOfComponent.propTypes = {text: React.PropTypes.string}
+
+// stateless functional component
+// const Button = (props) => {
+// 	return(
+// 		<button onClick={props.clickHandler}>{props.text}</button>
+// 	)
+// }
+
+// ES 6 Counter Challenge
+class NewCounter extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state = {count: 0}
+		this.increment = this.increment.bind(this)
+	}
+
+	increment(value){
 		this.setState({
-			count: this.state.count + 1
+			count: this.state.count + value
 		})
-	},
-	
-	render: function(){
-		return(
-			<button onClick={this.increment}>{this.state.count}</button>
-		)
-	},
-
-	componentDidMount: function(){
-		console.log('component has rendered')
-		this.interval = setInterval(this.increment, 1000)
-	},
-
-	componentWillUnmount: function(){
-		clearInterval(this.interval)
-		console.log('component unmounted')
+				
 	}
-})
-
-var LifecycleContainer = React.createClass({
-	mount: function(){
-		ReactDOM.render(
-			<LifeComponent />,
-			document.getElementById('renderHere')
-
-		)
-	},
-	unmount: function(){
-		ReactDOM.unmountComponentAtNode(
-			document.getElementById('renderHere')
-		)
-	},
 	
-	render: function(){
-		console.log('Lifecycle container rendered')
+	render(){
 		return(
-		<div>
-			<button onClick={this.mount}>Mount</button>
-			<button onClick={this.unmount}>Unmount</button>
-			<div id="renderHere"></div>
-		</div>
-		
+			<div>
+				<h1>{this.state.count}</h1>
+				<Button clickHandler={() => this.increment(this.props.valueOne)} text="OneMofo"></Button>
+				<Button clickHandler={() => this.increment(this.props.valueTwo)} text="OneMofo"></Button>
+			</div>
 		)
+
 	}
-})
+
+}
+
+NewCounter.defaultProps = {
+	valueOne: 1,
+	valueTwo: 5,
+	valueThree: 10,
+}
+
+const Button = (props) => {
+	return(
+		<button onClick={props.clickHandler}>{props.text}</button>
+	)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -162,7 +301,7 @@ var LifecycleContainer = React.createClass({
 
 ReactDOM.render(
 	<div>
-		<LifecycleContainer />
+		<NewCounter />
 	</div>,
 	destination
 )
--- scripts/build.js
+++ scripts/build.js
@@ -88,84 +88,204 @@ var destination = document.querySelector('#container'
 
 // Lifecycle of Components
 
-);var LifeComponent = React.createClass({
-	displayName: 'LifeComponent',
+// var LifeComponent = React.createClass({
+// 	getDefaultProps: function(){
+// 		console.log('getting default props')
+// 	},
 
-	getDefaultProps: function () {
-		console.log('getting default props');
-	},
+// 	getInitialState: function(){
+// 		return{
+// 			count: 0,
+// 		}
+// 	},
 
-	getInitialState: function () {
-		return {
-			count: 0
-		};
-	},
+// 	componentWillMount: function(){
+// 		console.log(this.state)
+// 		console.log(this.props)
+// 		console.log('component is mounting')
+// 	},
 
-	componentWillMount: function () {
-		console.log(this.state);
-		console.log(this.props);
-		console.log('component is mounting');
-	},
+// 	increment: function(){
+// 		this.setState({
+// 			count: this.state.count + 1
+// 		})
+// 	},
 
-	increment: function () {
-		this.setState({
-			count: this.state.count + 1
-		});
-	},
+// 	render: function(){
+// 		return(
+// 			<button onClick={this.increment}>{this.state.count}</button>
+// 		)
+// 	},
 
-	render: function () {
-		return React.createElement(
-			'button',
-			{ onClick: this.increment },
-			this.state.count
-		);
-	},
+// 	componentDidMount: function(){
+// 		console.log('component has rendered')
+// 		this.interval = setInterval(this.increment, 1000)
+// 	},
 
-	componentDidMount: function () {
-		console.log('component has rendered');
-		this.interval = setInterval(this.increment, 1000);
-	},
+// 	componentWillUnmount: function(){
+// 		clearInterval(this.interval)
+// 		console.log('component unmounted')
+// 	}
+// })
 
-	componentWillUnmount: function () {
-		clearInterval(this.interval);
-		console.log('component unmounted');
+// var LifecycleContainer = React.createClass({
+// 	mount: function(){
+// 		ReactDOM.render(
+// 			<LifeComponent />,
+// 			document.getElementById('renderHere')
+
+// 		)
+// 	},
+// 	unmount: function(){
+// 		ReactDOM.unmountComponentAtNode(
+// 			document.getElementById('renderHere')
+// 		)
+// 	},
+
+// 	render: function(){
+// 		console.log('Lifecycle container rendered')
+// 		return(
+// 		<div>
+// 			<button onClick={this.mount}>Mount</button>
+// 			<button onClick={this.unmount}>Unmount</button>
+// 			<div id="renderHere"></div>
+// 		</div>
+
+// 		)
+// 	}
+// })
+
+// Lifecycle Updates
+
+// var LikesComponent = React.createClass({
+// 	updateLikes: function(){
+// 		ReactDOM.render(
+// 			<LikesComponent likes={this.props.likes +1}/>,
+// 			destination
+// 		)
+// 	},
+
+// 	getDefaultProps: function(){
+// 		return({
+// 			likes: 0,
+// 		})
+// 	},
+
+// 	getInitialState: function(){
+// 		return({
+// 			popular: false,
+// 		})
+// 	},
+
+// 	componentWillReceiveProps: function(nextProps){
+// 		console.log('nex props')
+// 		this.setState({
+// 			popular: nextProps.likes >= 10
+// 		})
+// 	},
+
+// 	shouldComponentUpdate: function(nextProps,nextState){
+// 		return(
+// 			nextProps.likes % 2 === 0
+// 		)
+// 	},
+
+// 	render: function(){
+// 		return(
+// 			<div className="container">
+// 				<h1>{this.state.popular ? "I'm popular" : "not popular"}</h1>
+// 				<button onClick={this.updateLikes}>Likes: {this.props.likes}</button>
+// 			</div>
+// 		)
+// 	},
+
+// 	componentDidUpdate: function(prevProps,prevState){
+// 		console.log('previous props', prevProps)
+// 		console.log('previous states', prevState)
+// 	}
+
+// })
+
+// ES6 Classes
+
+// class NameOfComponent extends React.Component {
+// 	constructor(props){
+// 		super(props);
+// 		this.state = {count: 0}
+// 		this.click = this.click.bind(this)
+// 	}
+// 	click(){
+// 		console.log('clicked')
+// 	}
+// 	render(){
+// 		return(
+// <div onClick={ () => this.click(10) }>{this.props.text}</div>
+// 			<div className="container">
+// 				<Button clickHandler={this.click} text="Hello"></Button>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// NameOfComponent.defaultProps = {text: 'Bitch'}
+// NameOfComponent.propTypes = {text: React.PropTypes.string}
+
+// stateless functional component
+// const Button = (props) => {
+// 	return(
+// 		<button onClick={props.clickHandler}>{props.text}</button>
+// 	)
+// }
+
+// ES 6 Counter Challenge
+);class NewCounter extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { count: 0 };
+		this.increment = this.increment.bind(this);
 	}
-});
 
-var LifecycleContainer = React.createClass({
-	displayName: 'LifecycleContainer',
+	increment(value) {
+		this.setState({
+			count: this.state.count + value
+		});
+	}
 
-	mount: function () {
-		ReactDOM.render(React.createElement(LifeComponent, null), document.getElementById('renderHere'));
-	},
-	unmount: function () {
-		ReactDOM.unmountComponentAtNode(document.getElementById('renderHere'));
-	},
-
-	render: function () {
-		console.log('Lifecycle container rendered');
+	render() {
 		return React.createElement(
 			'div',
 			null,
 			React.createElement(
-				'button',
-				{ onClick: this.mount },
-				'Mount'
+				'h1',
+				null,
+				this.state.count
 			),
-			React.createElement(
-				'button',
-				{ onClick: this.unmount },
-				'Unmount'
-			),
-			React.createElement('div', { id: 'renderHere' })
+			React.createElement(Button, { clickHandler: () => this.increment(this.props.valueOne), text: 'OneMofo' }),
+			React.createElement(Button, { clickHandler: () => this.increment(this.props.valueTwo), text: 'OneMofo' })
 		);
 	}
-});
+
+}
+
+NewCounter.defaultProps = {
+	valueOne: 1,
+	valueTwo: 5,
+	valueThree: 10
+};
+
+const Button = props => {
+	return React.createElement(
+		'button',
+		{ onClick: props.clickHandler },
+		props.text
+	);
+};
 
 ReactDOM.render(React.createElement(
 	'div',
 	null,
-	React.createElement(LifecycleContainer, null)
+	React.createElement(NewCounter, null)
 ), destination);
 
 },{"react":182,"react-dom":31}],2:[function(require,module,exports){
